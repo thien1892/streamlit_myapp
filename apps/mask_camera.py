d0a9055dc86a98dd14ff8564013e239048fac9a6
thien1892 <thien1892@gmail.com>
2021-11-19 16:45:24 +0700

    Update mask_camera.py

--- apps/mask_camera.py
+++ apps/mask_camera.py
@@ -19,34 +19,35 @@ def app():
         def recv(self, frame):
             img = frame.to_ndarray(format="bgr24")
 
-            imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            faces = faceCascade.detectMultiScale(imgGray,1.4, 4, minSize=(30,30))
-            for (x,y,w,h) in faces:
-                # cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-                # ordering, resize it to 224x224, and preprocess it
-                face = img[y:y+h, x:x+w]
-                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                face = cv2.resize(face, (224, 224))
-                face = img_to_array(face)
-                face = preprocess_input(face)
-                face = np.expand_dims(face, axis=0)
+            flipped = img[::-1,:,:]
 
-                (mask, withoutMask) = model.predict(face)[0]
+            # imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            # faces = faceCascade.detectMultiScale(imgGray,1.4, 4, minSize=(30,30))
+            # for (x,y,w,h) in faces:
+            #     # cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            #     # ordering, resize it to 224x224, and preprocess it
+            #     face = img[y:y+h, x:x+w]
+            #     face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            #     face = cv2.resize(face, (224, 224))
+            #     face = img_to_array(face)
+            #     face = preprocess_input(face)
+            #     face = np.expand_dims(face, axis=0)
 
-                label = "mask" if mask > withoutMask else "no_mask"
-                color = (0, 255, 0) if label == "mask" else (255, 0, 0)
+            #     (mask, withoutMask) = model.predict(face)[0]
 
-                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-                cv2.putText(img, label, (x, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+            #     label = "mask" if mask > withoutMask else "no_mask"
+            #     color = (0, 255, 0) if label == "mask" else (255, 0, 0)
 
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
+            #     label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
+            #     cv2.putText(img, label, (x, y - 10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            #     cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+
+            return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
     ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
     s_value = st.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.05)
-
     # DEFAULT_CONFIDENCE_THRESHOLD = 0.5
     if ctx.video_processor:
         ctx.video_processor.some_value = s_value
