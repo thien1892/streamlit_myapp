be7b017aeabbd4f02c34fb3392b726237cbc4cca
thien1892 <thien1892@gmail.com>
2021-12-30 23:54:17 +0700

    update yolo img

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, mask_v2, yolo # import your app modules here
+from apps import home, mask_v2, yolo_img # import your app modules here
 
 st.markdown("""
 # Ứng dụng Teck with Thiện
@@ -16,7 +16,7 @@ app = MultiApp()
 
 # app.add_app("Nhận diện đeo khẩu trang", mask.app)
 app.add_app("Nhận diện đeo khẩu trang qua camera", mask_v2.app)
-app.add_app("Phát hiện vật thể với YOLO", yolo.app)
+app.add_app("Phát hiện vật thể với YOLO", yolo_img.app)
 app.add_app("Khám phá data", home.app)
 # The main app
 app.run()
