e98d4aad3ba113e2dd69772e906d13d5594c9202
thien1892 <thien1892@gmail.com>
2022-02-10 15:03:18 +0700

    delete home app

--- apps/home.py
+++ apps/home.py
@@ -1,16 +1,16 @@
-import streamlit as st
-import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+# import streamlit as st
+# import pandas as pd
+# import pandas_profiling
+# from streamlit_pandas_profiling import st_profile_report
 
-def app():
-    st.title('Khám phá data')
-    st.write('''
-    Ứng dụng này giúp bạn trực quan dữ liệu của bạn. Khai phá các mặt tổng quan của dữ liệu.
-    ''')
-    uploaded_file = st.file_uploader('Up load data của bạn', type= ['csv', 'xlsx'])
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        pr = df.profile_report()
-        st_profile_report(pr)
+# def app():
+#     st.title('Khám phá data')
+#     st.write('''
+#     Ứng dụng này giúp bạn trực quan dữ liệu của bạn. Khai phá các mặt tổng quan của dữ liệu.
+#     ''')
+#     uploaded_file = st.file_uploader('Up load data của bạn', type= ['csv', 'xlsx'])
+#     if uploaded_file is not None:
+#         df = pd.read_csv(uploaded_file)
+#         pr = df.profile_report()
+#         st_profile_report(pr)
 
